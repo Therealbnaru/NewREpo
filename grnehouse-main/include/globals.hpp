74ac6cc34607ae80fd29a611048be733c950e758
Therealbnaru <124009980+Therealbnaru@users.noreply.github.com>
2025-10-02 15:14:35 -0400

    dawd
    desadwa

---
+++ grnehouse-main/include/globals.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "EZ-Template/api.hpp"
+#include "api.h"
+
+extern pros::Motor intake;
+extern pros::Motor hood;
+extern pros::Motor hopper;
+
+
+
+//pneumatics
+extern pros::adi::Pneumatics hoodP;
+extern pros::adi::Pneumatics odomPull;
+extern pros::adi::Pneumatics redirect;
+
+extern pros::adi::Pneumatics trapDoor;
+
+
+
+
+//Sensors
+extern pros::Optical optical_sensorR;
+extern pros::Optical optical_sensorL;
+
+extern pros::Rotation rotation_sensor;
+extern pros::Distance distance_sensor;
+extern pros::adi::DigitalIn limitSwitch;
+
+//Variables
+extern bool intakeOff;
+extern bool spinCase;
+extern int alliance;
+extern int team;
+extern double hueR;
+extern double hueL;
+extern int proxL;
+extern int proxR;
+extern int globController;
